18e9d3e86d0f50436983387b04874c55b7646aba
spaezsuarez <spaezsuarez@gmail.com>
2020-10-12 21:45:08 -0500

    Radix sort terminado

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         RadixSort object = RadixSort.getInstance();
-        System.out.println(object.calcularOEFormula(100));
-        System.out.println(object.calcularOEContador(100));
+        System.out.println(object.calcularOEFormula(345));
+        System.out.println(object.calcularOEContador(345));
     }
 }
--- src/logic/RadixSort.java
+++ src/logic/RadixSort.java
@@ -17,60 +17,64 @@ public class RadixSort implements  Metodo {
         return  instancia;
     }
 
-    public int getMax(int arr[], int n)
-    {
-        int maximo = arr[0];
-        for (int i = 1; i < n; i++)
-            if (arr[i] > maximo)
-                maximo = arr[i];
-        return maximo;
-    }
-
-    public void countSort(int arr[], int n, int exp)
-    {
-
-    }
-
-
     @Override
     public int calcularOEFormula(int N) {
-        return 0;
+        return (85*N) + (231);
     }
 
     @Override
     public int calcularOEContador(int N) {
         int contador = 0;
-        int[] a = new int[N];
-        for(int i = 0; i < N; i++){
-            a[i] = N-i;
-        }
-        for(int element:a){
-            System.out.print(element+", ");
-        }
-        System.out.println("\n");
-        int m = getMax(a, N);
 
-        for (int exp = 1; m / exp > 0; exp *= 10) {
+        int[] a = new int[N];
+        for(int i = 0; i < N; i++)
+            a[i] = N-i;
+
+        int maximo = a[0];
+        contador += 2;
+
+        contador+= 2;
+        for (int i = 1; i < N; i++){//4(N-1)+4
+            contador += 4;
+            if (a[i] > maximo){
+                contador += 2;
+                maximo = a[i];
+            }
+        }
+
+        contador += 3; //
+        for (int exp = 1; maximo / exp > 0; exp *= 10) {
+            contador += 4;
+
             int output[] = new int[N];
             int i;
             int count[] = new int[10];
 
-            for (i = 0; i < N; i++)
+            for (i = 0; i < N; i++){ //7N + 2
                 count[(a[i] / exp) % 10]++;
+                contador += 7;
+            }
+            contador += 2;
 
-            for (i = 1; i < 10; i++)
+            for (i = 1; i < 10; i++){ //65
                 count[i] += count[i - 1];
+                contador += 7;
+            }
+            contador += 2;
 
-
-            for (i = N - 1; i >= 0; i--) {
+            for (i = N - 1; i >= 0; i--) { // 15N + 3
                 output[count[(a[i] / exp) % 10] - 1] = a[i];
                 count[(a[i] / exp) % 10]--;
+                contador += 15;
             }
+            contador += 3;
 
-            for (i = 0; i < N; i++)
+            for (i = 0; i < N; i++){ //5N +2 + 4
                 a[i] = output[i];
+                contador += 5;
+            }
+            contador += 2;
         }
-        //Fin de la implementacion
 
         return contador;
     }
